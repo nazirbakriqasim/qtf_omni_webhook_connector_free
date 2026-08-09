816aa16fda2b5b205c36aaec0f517c7a9c409020
Nazir Qasim <nazeeer82@gmail.com>
2026-08-09 17:46:56 +0300

    start

--- qtf_omni_webhook_connector_free/__manifest__.py
+++ qtf_omni_webhook_connector_free/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Omni Webhook & Messenger Connector (Free) By QTF',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'sequence': 10,
     'category': 'Discuss/Marketing',
     'summary': 'Bridge Odoo Discuss Messages with Any Webhook API, Telegram, or WhatsApp Gateways',
-    'description': 'A completely free, open-source universal webhook engine developed by QTF to forward Odoo 19 internal chat messages to any messaging gateway or API endpoint dynamically.',
+    'description': 'A completely free, open-source universal webhook engine developed by QTF to forward Odoo 18 internal chat messages to any messaging gateway or API endpoint dynamically.',
     'author': 'Nazir Bakri Qasim',
     'website': 'https://wa.me/nazirbakriqasim',
     'license': 'LGPL-3',
